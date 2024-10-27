99bf0821f5437de524cc4d8d57cbdbf5636e1dd3
nhan 295 <tranthihongnhan295@gmail.com>
2024-10-27 17:27:51 +0700

    add homepage interface

---
+++ frontend/src/view/HomePage.vue
@@ -0,0 +1,36 @@
+<template>
+     
+   <HomePage_Header />
+
+     <div class="title" style="padding-top: 60px;">
+       
+        <ListBook />
+     </div>
+
+     <div class="title" style="padding-top: 30px;">
+        
+        <ListBook />
+     </div>
+
+     <div class="title" style="padding-top: 30px;">
+        
+        <ListBook />
+     </div>
+    
+     <div class="title" style="padding-top: 30px;">
+        
+        <ListBook />
+     </div>
+</template>
+
+<script>
+import HomePage_Header from '@/components/HomePage_Header.vue';
+import ListBook from '@/components/ListBook.vue';
+    export default {
+        components: {
+            HomePage_Header,
+            ListBook,
+        }
+
+    };
+</script>
---
+++ frontend/src/view/UserPage.vue
@@ -0,0 +1,27 @@
+<template>
+     <UserPage_Header />
+
+     <div class="title" style="padding-top: 60px;">
+        <ListBook />
+     </div>
+
+     <div class="title" style="padding-top: 30px;">
+       
+        <ListBook />
+     </div>
+</template>
+
+<script>
+import UserPage_Header from '@/components/UserPage_Header.vue';
+import ListBook from '@/components/ListBook.vue';
+    export default {
+        components: {
+            UserPage_Header,
+            ListBook,
+        }
+
+    };
+</script>
+
+<style>
+</style>
